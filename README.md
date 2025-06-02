8924e20e3cda3e63f1e02d1788beb71c79f5197b
zyx <2511305125@qq.com>
2025-06-02 01:01:24 +0100

    Fix executable name in CMakeLists.txt and update README for installation instructions

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Libgpiod-test-on-rock-5B
+# Installation
+```bash
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+```
+
+# Description
+## Servo motor setting
+Add to '/boot/firmware/config.txt':
+```
+dtoverlay=pwm-2chan
+```
+Check with:
+```
+pinctrl -p
+```
--- main.cpp
+++ main.cpp
@@ -7,13 +7,15 @@ int main(int argc, char *argv[])
         std::cout << "Usage: " << argv[0] << " <left_speed> <right_speed>" << std::endl;
         return 1;
     }
+
+    //The parameters are (channel, frequency, duty cycle, chip No)
     ServoMotorSetting servo_left(0, 20, 7.5, 14);
     ServoMotorSetting servo_right(0, 20, 7.5, 8);
 
     int left_speed = std::atoi(argv[1]);
     int right_speed = std::atoi(argv[2]);
 
-    std::cout << "The speed of the left wheel is " << left_speed << " ,the speed of the right wheel is " << right_speed << std::endl;
+    std::cout << "The speed of the left wheel is " << left_speed << ", the speed of the right wheel is " << right_speed << std::endl;
 
     servo_left.ChangeSpeed(left_speed);
     servo_right.ChangeSpeed(right_speed);
