3cddacc0478014376b12940629e536749600c950
zyx <2511305125@qq.com>
2025-06-05 01:48:34 +0100

    fix: improve clarity in getch function comments and output messages

--- Tests/TestBotMotion.cpp
+++ Tests/TestBotMotion.cpp
@@ -6,16 +6,20 @@
 
 using namespace std;
 
+/**
+ * Reads individual character input directly from the keyboard without the need for a carriage return.
+ * @return {char}  : input character
+ */
 char getch()
 {
-    struct termios oldt, newt;
+    struct termios oldt, newt; // Save settings of old and new terminal
     char ch;
-    tcgetattr(STDIN_FILENO, &oldt);
+    tcgetattr(STDIN_FILENO, &oldt); // Get current terminal settings
     newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    ch = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    newt.c_lflag &= ~(ICANON | ECHO); // Close canonical mode and echo (Do not need to press enter and the character will not be displayed on the screen)
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Apply new settings
+    ch = getchar(); // Get one character
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Restore original settings
     return ch;
 }
 
@@ -53,13 +57,13 @@ int main(int argc, char *argv[])
             speed++;
             if (speed >= 10)
                 speed = 10;
-            cout << "Speed up.....The speed is" << speed << endl;
+            cout << "Speed up.....The speed is: " << speed << endl;
             break;
         case 'f':
             speed--;
             if (speed <= 0)
                 speed = 0;
-            cout << "Declerate.....The speed is" << speed << endl;
+            cout << "Declerate.....The speed is: " << speed << endl;
             break;
 
         case 'x':
@@ -69,7 +73,7 @@ int main(int argc, char *argv[])
 
         case 'z':
             motion.Stop();
-            cout << "Quit......" << endl;
+            cout << "Quit....." << endl;
             exit(0);
 
         default:
