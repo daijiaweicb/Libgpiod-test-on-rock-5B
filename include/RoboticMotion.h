c378b8c5229845fe69505cb16d979904b1759396
zyx <2511305125@qq.com>
2025-06-10 01:19:54 +0100

    fix: enhance documentation for Motion class methods in RoboticMotion.h

--- include/RoboticMotion.h
+++ include/RoboticMotion.h
@@ -6,12 +6,45 @@
 class Motion
 {
 public:
+    /**
+     * Motion 
+     * 
+     * @param  {int} leftChannel  : The GPIO pin number for the left motor
+     * @param  {int} rightChannel : The GPIO pin number for the right motor
+     * @param  {float} low_time   : The low time duration for the PWM signal
+     * @param  {float} high_time  : The high time duration for the PWM signal
+     * @param  {int} rightChipNo  : The chip number for the right motor
+     * @param  {int} leftChipNo   : The chip number for the left motor
+     */
     Motion(int leftChannel, int rightChannel, float low_time, float high_time, int rightChipNo, int leftChipNo);
 
+    /**
+     * Move the robot forward.
+     * @param  {float} speed : The speed to move forward.
+     */
     void Forward(float speed);
+
+    /**
+     * Move the robot backward.
+     * @param  {float} speed : The speed to move backward.
+     */
     void Backward(float speed);
+
+    /**
+     * Move the robot to the left.
+     * @param  {float} speed : The speed to move to the left.
+     */
     void TurnLeft(float speed);
+
+    /**
+     * Move the robot to the right.
+     * @param  {float} speed : The speed to move to the right.
+     */
     void TurnRight(float speed);
+
+    /**
+     * Stop the robot.
+     */
     void Stop();
 
 private:
